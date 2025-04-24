b1b37f779cbec5db207800d3863d269706e8200f
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-24 14:57:59 +0500

    added removing from database users in admin dashboard

--- src/pages/Admin/components/cards/UserCard.ts
+++ src/pages/Admin/components/cards/UserCard.ts
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { API_URL } from "../../../../constants/constants";
+
+declare global {
+  interface Window {
+    adminDeleteUser: (...params: any) => void;
+  }
+}
+
 export const UserCard = (userData: any): string => {
+  window.adminDeleteUser = (_this: any, id: any) => {
+    console.log(id);
+
+    axios
+      .delete(`${API_URL}/users/${id}`)
+      .then(() => {
+        _this.parentElement?.remove();
+        location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return /*html*/ `
     <div class="bg-white p-5 min-h-[300px] min-w-[250px] rounded-md flex flex-col items-center mt-8 dark:bg-sm-black-light">
         <div class="h-30 w-30 bg-orange-500 rounded-full text-white flex items-center justify-center text-4xl font-extrabold select-none mt-[-3rem] outline-[10px] outline-sm-white dark:outline-sm-black">${String(
@@ -26,6 +49,9 @@ export const UserCard = (userData: any): string => {
               })
               .join("")}
         </div>
+        <button class="w-full bg-red-500 text-white mt-auto p-1 rounded-md" onclick="window.adminDeleteUser(this, '${
+          userData.id
+        }')">Delete</button>
     </div>
     `;
 };
