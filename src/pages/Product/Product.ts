7945065845fcc8249d764c7d6e0bfe599fa26e7b
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-24 16:11:55 +0500

    fix product's description in dark mode in Product page

--- src/pages/Product/Product.ts
+++ src/pages/Product/Product.ts
@@ -40,7 +40,7 @@ export const Product = (id: string): string => {
                     <div>
                         <h1 class="text-6xl font-medium dark:text-white max-2xl:text-4xl"></h1>
                         <h2 class="text-sm-gray-light text-4xl mt-4 max-2xl:text-2xl"></h2>
-                        <p class="max-w-[400px] mt-3 max-2xl:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores tempora corrupti consectetur, et illum animi a autem repellat repudiandae</p>
+                        <p class="max-w-[400px] mt-3 max-2xl:text-sm dark:text-sm-gray-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores tempora corrupti consectetur, et illum animi a autem repellat repudiandae</p>
                         <button type="button" class="bg-sm-purple text-white p-3 rounded-xl mt-5 text-xl tranistion duration-200 hover:opacity-90 max-2xl:text-lg max-2xl:p-2">Add to cart</button>
                         <div>
                         
