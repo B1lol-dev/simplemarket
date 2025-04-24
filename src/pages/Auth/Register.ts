d86b0b64bb0a660b50589fbbc6cbc73c7fb10a2e
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-24 15:58:29 +0500

    added responsive design to login and register pages

--- src/pages/Auth/Register.ts
+++ src/pages/Auth/Register.ts
@@ -50,11 +50,11 @@ export const Register = (): string => {
   return /*html*/ `
     <div class="h-screen w-screen flex items-center justify-center bg-sm-white dark:bg-sm-black">
         <button onclick="history.back()" class="fixed top-3 left-3 bg-sm-purple w-10 h-10 rounded-md text-white"><-</button>
-        <form class="flex flex-col gap-4 bg-white p-10 rounded-md shadow-md dark:bg-sm-black-light" id="register_form">
-            <input type="text" placeholder="username" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white" required>
-            <input type="password" placeholder="password" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white" required>
-            <input type="password" placeholder="retype password" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white" required>
-            <button class="bg-sm-purple text-white p-1 text-2xl rounded-md">Register</button>
+        <form class="flex flex-col gap-4 bg-white p-10 rounded-md shadow-md dark:bg-sm-black-light max-lg:p-6" id="register_form">
+            <input type="text" placeholder="username" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white max-lg:text-xl" required>
+            <input type="password" placeholder="password" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white max-lg:text-xl" required>
+            <input type="password" placeholder="retype password" class="px-3 py-1 border border-sm-gray rounded-md text-2xl user-invalid:border-red-500 dark:text-white max-lg:text-xl" required>
+            <button class="bg-sm-purple text-white p-1 text-2xl rounded-md max-lg:text-xl">Register</button>
             <a href="/login" class="text-sm-aqua" onclick="location.pathname = '/login'">Already have an account?</a>
         </form>
     </div>
